73c87c02102dcbf3d67f9bee8aead34050374645
Kyrylo Chernyavskyi <kiril.chwork@gmail.com>
2021-02-25 22:36:27 +0200

    creation adaptive layout, add some animations and transitions

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ $( function() {
         $(this).toggleClass("visible");
         $( ".about-section__chronology-list" ).toggle(400);
     } );
+    $("nav").on("click","a", function (event) {
+        event.preventDefault();
+        let id = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
 } );
