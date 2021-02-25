6f1a58bb8b9a82f8af088bd83338a7813d675350
Kyrylo Chernyavskyi <kiril.chwork@gmail.com>
2021-02-25 23:20:33 +0200

    highlighting a menu item with scrolling

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-$( function() {
-    $( ".about-section__chronology" ).click( function() {
-        $(this).toggleClass("visible");
-        $( ".about-section__chronology-list" ).toggle(400);
-    } );
-    $("nav").on("click","a", function (event) {
-        event.preventDefault();
-        let id = $(this).attr('href'),
-        top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1000);
+$(function () {
+  $(".about-section__chronology").click(function () {
+    $(this).toggleClass("visible");
+    $(".about-section__chronology-list").toggle(400);
+  });
+  $("nav").on("click", "a", function (event) {
+    event.preventDefault();
+    let id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1000);
+  });
+  let sections = $("section"),
+    nav = $("nav");
+  $(window).on("scroll", function () {
+    let cur_pos = $(this).scrollTop();
+
+    sections.each(function () {
+      let top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        nav.find("a").removeClass("active");
+        nav.find('a[href="#' + $(this).attr("id") + '"]').addClass("active");
+      }
     });
-} );
+  });
+});
